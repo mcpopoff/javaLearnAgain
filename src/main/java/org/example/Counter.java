0d0095d1f76a0485f0ff1f8bec6597889d4725a3
mcpopoff <mcpopoff@ya.ru>
2023-03-14 18:55:47 +0500

    fisrt commit

---
+++ src/main/java/org/example/Counter.java
@@ -0,0 +1,42 @@
+package org.example;
+
+public class Counter {
+    private final String NAME;
+    private int value;
+
+    Counter(String name, int value){
+        NAME = name;
+        this.value = value;
+    }
+    Counter(String name){
+        this(name, 0);
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public int increase(int i){
+        value += i;
+        return getValue();
+    }
+    public int increase(){
+        return increase(1);
+    }
+
+    public int decrease(int i){
+        value -= i;
+        return getValue();
+    }
+    public int decrease(){
+        return decrease(1);
+    }
+
+    public String getName() {
+        return NAME;
+    }
+    @Override
+    public String toString(){
+        return getName() + ": " + getValue();
+    }
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,19 @@
+package org.example;
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("----LESSON_008----");
+        Counter e = new Counter("evenCounter");
+        Counter o = new Counter("oddCounter");
+        for (int i = 1; i <= 100; i++){
+            if (i % 2 == 0){
+                e.increase();
+            } else {
+                o.increase();
+            }
+        }
+        System.out.println(e);
+        System.out.println(o);
+    }
+}
